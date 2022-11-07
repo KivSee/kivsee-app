eb4c0efff25035dbf8288c27715fa1430314c293
Amir Blum <amir@aspecto.io>
2022-11-07 21:05:39 +0200

    feat: hello world with react

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/app/index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3456,
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/app/index.ejs',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: 'ts-loader',
+                },
+            },
+        ],
+    },
+};
